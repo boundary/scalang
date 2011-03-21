3f851934b0ff1206a4973b186700b6e38a0609eb
cliff moon <cliff@leaninto.it>
2011-03-21 13:30:10 -0700

    add log4j deps

--- project/build/ScalangProject.scala
+++ project/build/ScalangProject.scala
@@ -9,5 +9,10 @@ class ScalangProject(info : ProjectInfo) extends DefaultProject(info) {
   val jetlang = "org.jetlang" % "jetlang" % "0.2.5"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test"
   
+  //logging
+  val log4j = "log4j" % "log4j" % "1.2.16"
+  val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.8"
+  val slf4j = "org.slf4j" % "slf4j-log4j12" % "1.5.8"
+  
   val publishTo = Resolver.ssh("fastipInternal", "apt.dfw2.fastip.com", "/srv/reprepro_internal/ivy")
 }

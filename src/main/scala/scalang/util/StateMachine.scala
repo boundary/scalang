e3925b0089551778efe187065de452e2925a150f
cliff moon <cliff@leaninto.it>
2011-03-21 14:07:23 -0700

    cute little statemachine impl for scala

---
+++ src/main/scala/scalang/util/StateMachine.scala
@@ -0,0 +1,54 @@
+package scalang.util
+
+/**
+ * State Machine
+ * example usage:
+ * 
+ */
+
+trait StateMachine {
+  
+  def start : Symbol
+  val mutex = new Object
+  protected var stateList : List[State] = null
+  @volatile protected var currentState = start
+  
+  def event(evnt : Any) {
+    mutex.synchronized {
+      if (currentState == null) {
+        currentState = start
+      }
+      println("stateList " + stateList)
+      println("current state " + currentState)
+      val state = stateList.find(_.name == currentState).getOrElse(throw new UndefinedStateException("state " + currentState + " is undefined"))
+      println("state " + state)
+      val nextState = state.event(evnt)
+      stateList.find(_.name == nextState).getOrElse(throw new UndefinedStateException("state " + currentState + " is undefined"))
+      currentState = nextState
+    }
+  }
+  
+  protected def states(states : State*) {
+    println("states " + states)
+    stateList = states.toList
+  }
+  
+  protected def state(name : Symbol, transitions : PartialFunction[Any,Symbol]) = State(name, transitions)
+  
+  case class State(name : Symbol, transitions : PartialFunction[Any,Symbol]) {
+    def event(evnt : Any) : Symbol = {
+      println("state " + name + " event " + evnt)
+      if (!transitions.isDefinedAt(evnt)) {
+        val ex =  new UnexpectedEventException("State " + name + " does not have a transition for event " + evnt)
+        println("ex " + ex.getMessage)
+        throw ex
+      } else {
+        transitions(evnt)
+      }
+    }
+  }
+  
+  class UnexpectedEventException(msg : String) extends Exception
+  
+  class UndefinedStateException(msg : String) extends Exception
+}

aed4be7ecd2378f76adeda12dfbe2f05bc3c2567
cliff moon <cliff@leaninto.it>
2011-03-21 23:30:36 -0700

    handshake decoder

---
+++ src/main/scala/scalang/node/HandshakeDecoder.scala
@@ -0,0 +1,54 @@
+package scalang.node
+
+import org.jboss.{netty => netty}
+import netty.handler.codec.oneone._
+import netty.channel._
+import netty.buffer._
+
+class HandshakeDecoder extends OneToOneDecoder {
+  
+  //we need to have a dirty fucking mode context
+  //because name messages and challenge replies have
+  //the same identifier
+  @volatile var mode = 'name
+  
+  def decode(ctx : ChannelHandlerContext, channel : Channel, obj : Any) : Object = {
+    //dispatch on first byte
+    val buffer = obj.asInstanceOf[ChannelBuffer]
+    (mode, buffer.readByte) match {
+      case ('name, 110) => //name message
+        val version = buffer.readShort
+        val flags = buffer.readInt
+        val nameLength = buffer.readableBytes
+        val bytes = new Array[Byte](nameLength)
+        buffer.readBytes(bytes)
+        mode = 'challenge
+        NameMessage(version, flags, new String(bytes))
+      case ('challenge, 110) => //challenge message
+        val version = buffer.readShort
+        val flags = buffer.readInt
+        val challenge = buffer.readInt
+        val nameLength = buffer.readableBytes
+        val bytes = new Array[Byte](nameLength)
+        buffer.readBytes(bytes)
+        ChallengeMessage(version, flags, challenge)
+      case (_, 115) => //status message
+        val statusLength = buffer.readableBytes
+        val bytes = new Array[Byte](statusLength)
+        buffer.readBytes(bytes)
+        StatusMessage(new String(bytes))
+      case (_, 114) => //reply message
+        val challenge = buffer.readInt
+        val digestLength = buffer.readableBytes
+        val bytes = new Array[Byte](digestLength)
+        buffer.readBytes(bytes)
+        ChallengeReplyMessage(challenge, bytes)
+      case (_, 97) => //ack message
+        val digestLength = buffer.readableBytes
+        val bytes = new Array[Byte](digestLength)
+        buffer.readBytes(bytes)
+        ChallengeAckMessage(bytes)
+    }
+  }
+  
+}
---
+++ src/test/scala/scalang/node/HandshakeDecoderSpec.scala
@@ -0,0 +1,71 @@
+package scalang.node
+
+import org.specs._
+import scalang.util._
+import org.jboss.{netty => netty}
+import netty.handler.codec.embedder._
+import netty.buffer.ChannelBuffers._
+
+class HandshakeDecoderSpec extends Specification {
+  "HandshakeDecoder" should {
+    "decode name messages" in {
+      val decoder = new HandshakeDecoder
+      val embedder = new DecoderEmbedder[NameMessage](decoder)
+      
+      val bytes = ByteArray(110, 0,5, 0,0,127,253, 116,109,112,64,98,108,97,104)
+      val buffer = copiedBuffer(bytes)
+      embedder.offer(buffer)
+      val msg = embedder.poll
+      msg must ==(NameMessage(5, 32765, "tmp@blah"))
+      
+      decoder.mode must ==('challenge) //decoding a name message should trigger a state change
+    }
+    
+    "decode status messages" in {
+      val decoder = new HandshakeDecoder
+      val embedder = new DecoderEmbedder[StatusMessage](decoder)
+      
+      val bytes = ByteArray(115, 111,107)
+      val buffer = copiedBuffer(bytes)
+      embedder.offer(buffer)
+      val msg = embedder.poll
+      msg must ==(StatusMessage("ok"))
+    }
+    
+    "decode challenge messages" in {
+      val decoder = new HandshakeDecoder
+      val embedder = new DecoderEmbedder[ChallengeMessage](decoder)
+      decoder.mode = 'challenge
+      val bytes = ByteArray(110, 0,5, 0,0,127,253, 0,1,56,213, 116,109,112,64,98,108,97,104)
+      val buffer = copiedBuffer(bytes)
+      embedder.offer(buffer)
+      val msg = embedder.poll
+      msg must ==(ChallengeMessage(5, 32765, 80085))
+    }
+    
+    "decode reply messages" in {
+      val decoder = new HandshakeDecoder
+      val embedder = new DecoderEmbedder[ChallengeReplyMessage](decoder)
+      val bytes = ByteArray(114, 0,1,56,213, 112,111,111,111,111,111,111,111,111,111,111,111,111,111,111,112)
+      val buffer = copiedBuffer(bytes)
+      embedder.offer(buffer)
+      val msg = embedder.poll
+      msg must beLike { case ChallengeReplyMessage(80085, digest) =>
+        digest.deep == ByteArray(112,111,111,111,111,111,111,111,111,111,111,111,111,111,111,112).deep
+      }
+    }
+    
+    "decode ack messages" in {
+      val decoder = new HandshakeDecoder
+      val embedder = new DecoderEmbedder[ChallengeAckMessage](decoder)
+      val bytes = ByteArray(97, 112,111,111,111,111,111,111,111,111,111,111,111,111,111,111,112)
+      val buffer = copiedBuffer(bytes)
+      embedder.offer(buffer)
+      val msg = embedder.poll
+      msg must beLike { case ChallengeAckMessage(digest) =>
+        println("digest " + digest.deep)
+        digest.deep == ByteArray(112,111,111,111,111,111,111,111,111,111,111,111,111,111,111,112).deep
+      }
+    }
+  }
+}

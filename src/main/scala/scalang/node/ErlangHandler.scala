6bafd771864852f3a7eaf1c2c960505a47044cea
cliff moon <cliff@leaninto.it>
2011-06-11 22:05:07 -0700

    use the remoteBreak function to deliver breakages from remote nodes

--- src/main/scala/scalang/node/ErlangHandler.scala
+++ src/main/scala/scalang/node/ErlangHandler.scala
@@ -9,12 +9,15 @@ import channel._
 import scalang._
 import com.codahale.logula.Logging
 
-class ErlangHandler(node : ErlangNode) extends SimpleChannelUpstreamHandler with Logging {
+class ErlangHandler(
+    node : ErlangNode, 
+    afterHandshake : Channel => Unit = { _ => Unit }) extends SimpleChannelUpstreamHandler with Logging {
   
   @volatile var peer : Symbol = null
   
   override def exceptionCaught(ctx : ChannelHandlerContext, e : ExceptionEvent) {
     log.error(e.getCause, "error caught in erlang handler")
+    ctx.getChannel.close
   }
   
   override def messageReceived(ctx : ChannelHandlerContext, e : MessageEvent) {
@@ -25,17 +28,19 @@ class ErlangHandler(node : ErlangNode) extends SimpleChannelUpstreamHandler with
         ctx.getChannel.write(Tock) //channel heartbeat for erlang
       case HandshakeFailed(name) =>
         //not much we can do here?
+        ctx.getChannel.close
       case HandshakeSucceeded(name, channel) =>
         peer = name
         node.registerConnection(name, channel)
+        afterHandshake(channel)
       case LinkMessage(from, to) =>
         node.link(from, to)
       case SendMessage(to, msg) =>
         node.handleSend(to, msg)
       case ExitMessage(from, to, reason) =>
-        node.break(from, to, reason)
+        node.remoteBreak(Link(from, to), reason)
       case Exit2Message(from, to, reason) =>
-        node.break(from, to, reason)
+        node.remoteBreak(Link(from, to), reason)
       case UnlinkMessage(from, to) =>
         node.unlink(from, to)
       case RegSend(from, to, msg) =>

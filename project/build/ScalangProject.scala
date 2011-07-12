a9be6df08d01073d75fb324360ba9b6d9eb6e7dd
cliff moon <cliff@leaninto.it>
2011-07-11 23:41:59 -0700

    update to external repo

--- project/build/ScalangProject.scala
+++ project/build/ScalangProject.scala
@@ -8,8 +8,7 @@ class ScalangProject(info : ProjectInfo) extends DefaultProject(info) {
   override def managedStyle = ManagedStyle.Maven
   val boundaryPublic = "Boundary Public Repo" at "http://maven.boundary.com/artifactory/repo"
   
-/*  val publishTo = "Boundary Public Repo (Publish)" at "http://maven.boundary.com/artifactory/external"*/
-  val publishTo = "Boundary Private Repo" at "http://maven.eng.boundary.com/artifactory/external"
+  val publishTo = "Boundary Public Repo (Publish)" at "http://maven.boundary.com/artifactory/external"
   
   
   val logula = "com.codahale" %% "logula" % "2.1.2"
@@ -18,5 +17,5 @@ class ScalangProject(info : ProjectInfo) extends DefaultProject(info) {
   val overlock = "com.boundary" %% "overlock" % "0.5"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test"
   
-  Credentials(Path.userHome / ".ivy2" / ".credentials-internal", log)
+  Credentials(Path.userHome / ".ivy2" / ".credentials-external", log)
 }
